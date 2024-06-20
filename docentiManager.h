4312d02aaf9264fb174b5e0027b26d3c24333059
anter03 <64782750+anter03@users.noreply.github.com>
2024-06-20 19:19:53 +0200

    resettiamo

--- docentiManager.h
+++
@@ -1,8 +0,0 @@
-/** @brief crea un nuovo nodo Docente */
-void CreateNode();
-
-/** @brief elimina un nodo data la posizione */
-void DropNodeByIndex(int pos);
-
-/** @brief stampa tutta la lista dei docenti   */
-void stampaLista();
--- funzioni.h
+++
@@ -1,6 +0,0 @@
-#ifndef FUNZIONI_H
-#define FUNZIONI_H
-void menu();
-void cleanConsole();
-
-#endif
